dbe88f553094cf94d68bdd3a4db4e80d4663b0f6
Laurence Horrocks-Barlow <lhorrocks-barlow@ocf.co.uk>
2020-04-13 19:01:06 +0100

    increasing z offset range

--- Working/Bowden - Marlin 1.1.9 - 1 extruder - BLTouch/Marlin/Configuration.h
+++ Working/Bowden - Marlin 1.1.9 - 1 extruder - BLTouch/Marlin/Configuration.h
@@ -822,8 +822,8 @@
 #define Z_PROBE_LOW_POINT          -1 // Farthest distance below the trigger-point to go before stopping
 
 // For M851 give a range for adjusting the Z probe offset
-#define Z_PROBE_OFFSET_RANGE_MIN -5
-#define Z_PROBE_OFFSET_RANGE_MAX 5
+#define Z_PROBE_OFFSET_RANGE_MIN -10
+#define Z_PROBE_OFFSET_RANGE_MAX 10
 
 // Enable the M48 repeatability test to test probe accuracy
 //#define Z_MIN_PROBE_REPEATABILITY_TEST
